5dda827ad407bf10312b824db02e165c75448a0a
sanghunmoon <lego0142@naver.com>
2023-09-29 01:23:41 +0900

    feat: endpoint 에 health 추가

--- webapp/endpoints.py
+++ webapp/endpoints.py
@@ -1,3 +1,26 @@
-from fastapi import APIRouter
+import time
 
-router = APIRouter()
+from fastapi import APIRouter, Response
+from fastapi.routing import APIRoute
+
+
+class TimedRoute(APIRoute):
+    def get_route_handler(self):
+        original_route_handler = super().get_route_handler()
+
+        async def custom_route_handler(request):
+            before = time.time()
+            response: Response = await original_route_handler(request)
+            duration = time.time() - before
+            response.headers["X-Response-Time"] = str(duration)
+            return response
+
+        return custom_route_handler
+
+
+router = APIRouter(route_class=TimedRoute)
+
+
+@router.get("/health")
+async def health():
+    return {"status": "ok"}
