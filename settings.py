1b3ba2084412d28a2bf5b0725b1d9e8ddc99c160
sanghunmoon <lego0142@naver.com>
2023-11-12 00:18:13 +0900

    feat: add mysql settings

--- settings.py
+++ settings.py
@@ -11,6 +11,7 @@ load_dotenv()
 class ProjectSettings(BaseSettings):
     OPENAI_API_KEY: str = Field(..., env="OPENAI_API_KEY")
     SENTRY_DSN: str = Field(..., env="SENTRY_DSN")
+    SQLALCHEMY_DATABASE_URL: str = Field(..., env="SQLALCHEMY_DATABASE_URL")
 
 
 settings = ProjectSettings()
