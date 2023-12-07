689396d4cb0bc0d27d14131d16ead1b10f27dedf
sanghunmoon <lego0142@naver.com>
2023-12-08 03:35:25 +0900

    feat: add pinecone logic

--- t_backend/repositories/openai.py
+++ t_backend/repositories/openai.py
@@ -1,13 +1,18 @@
+import os
+
+import pinecone
 from langchain.chat_models import ChatOpenAI
 from langchain.chat_models.base import (
     BaseChatModel,
 )
+from langchain.embeddings import OpenAIEmbeddings
 from langchain.prompts import (
     SystemMessagePromptTemplate,
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
 )
 from langchain.schema.messages import BaseMessage
+from langchain.vectorstores import Pinecone
 from redis import Redis
 
 from t_backend.langchain.prompts import SYSTEM_TEMPLATE, HUMAN_TEMPLATE
@@ -35,6 +40,7 @@ class OpenAIRepository:
         )
 
     def send_message(self, message_id: int, messages: list[BaseMessage]) -> str:
+        messages.append(BaseMessage(content=self.get_paper(messages[-1].content)))
         chat_prompt_template = ChatPromptTemplate.from_messages(
             [self.system_message_prompt_template, self.human_message_prompt_template]
         )
@@ -47,3 +53,19 @@ class OpenAIRepository:
             full_content += chunk.content
             self._redis_client.set(str(message_id), full_content)
         return full_content
+
+    def get_paper(self, content: str) -> str:
+        index_name = "tiro-papers-1"
+        pinecone.init(
+            api_key=os.environ.get("pinecone_api_key"), environment="us-east-1-aws"
+        )
+        embedding = OpenAIEmbeddings(
+            model_name="text-embedding-ada-002", openai_api_key=settings.OPENAI_API_KEY
+        )
+        index = Pinecone.from_existing_index(
+            index_name=index_name,
+            embedding=embedding,
+            namespace=index_name,
+        )
+        res = index.similarity_search(query=content)
+        return res[0].page_content
